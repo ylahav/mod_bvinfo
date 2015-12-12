fa3d0677a030c4dd50b0f4f085018aecf8bfbd7b
yair lahav <yair@billrun.net>
2015-12-12 13:04:58 +0200

    apply JED check remarks...

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * @author
- * @copyright
- * @license
+ * @package     Joomla.Site
+ * @subpackage  mod_bvinfo
+ *
+ * @copyright   Copyright (C) 2015 Yair Lahav, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined("_JEXEC") or die("Restricted access");
